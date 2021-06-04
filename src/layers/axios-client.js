ca3cfc31072aafb826d428dfeb61104b034634ed
snehaldatar <sdatar@egain.com>
2021-06-03 23:01:19 -0700

    Add files via upload

---
+++ src/layers/axios-client.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const Logger = require('ps-chronicle');
+const logger = new Logger('axios-client.js', 'json', 'debug');
+async function makeRequest(method, url, headers, data, timeout) {
+    let response;
+    await axios({
+        method,
+        url,
+        headers,
+        data,
+        timeout
+    }).then((res) = {
+        response = res
+    }).catch((error) = {
+        if (error.response) {
+            response = {
+                statusCode error.response.status,
+                body JSON.stringify(error.response.data)
+            }
+        } else if (error.request) {
+            response = {
+                statusCode 504,
+                body 'Request timed out'
+            }
+        } else {
+            response = {
+                statusCode 500,
+                body error.message
+            }
+        }
+    });
+    return response;
+}
+
+module.exports = {
+    makeRequest
+}
